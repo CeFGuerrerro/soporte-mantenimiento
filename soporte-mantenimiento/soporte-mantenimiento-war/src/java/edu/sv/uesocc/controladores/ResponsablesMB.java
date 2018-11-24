1203c42b52b7022dd68a92658c389f67a4f981d0
Carlos Fuentes <o-a19@CEF-LAPTOP>
2018-11-23 21:00:17 -0600

    Formulario Responsables agregado, formulario Equipos modificado

--- soporte-mantenimiento/soporte-mantenimiento-war/src/java/edu/sv/uesocc/controladores/ResponsablesMB.java
+++ soporte-mantenimiento/soporte-mantenimiento-war/src/java/edu/sv/uesocc/controladores/ResponsablesMB.java
@@ -5,9 +5,17 @@
  */
 package edu.sv.uesocc.controladores;
 
+import edu.sv.uesocc.entidades.Responsables;
+import edu.sv.uesocc.facades.ResponsablesFacadeLocal;
 import javax.inject.Named;
 import javax.enterprise.context.SessionScoped;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
 
 /**
  *
@@ -17,10 +25,99 @@ import java.io.Serializable;
 @SessionScoped
 public class ResponsablesMB implements Serializable {
 
-    /**
-     * Creates a new instance of ResponsablesMB
-     */
+    @EJB
+    private ResponsablesFacadeLocal responsablesFacade;
+
+    private Responsables resp;
+    private Responsables respSeleccionado;
+    private List<Responsables> respList = new ArrayList<>();
+    
     public ResponsablesMB() {
     }
     
+    @PostConstruct
+    public void init(){
+        obtenerTodos();
+        resp = new Responsables();
+        respSeleccionado = new Responsables();
+    }
+
+    public Responsables getResp() {
+        return resp;
+    }
+
+    public void setResp(Responsables resp) {
+        this.resp = resp;
+    }
+
+    public Responsables getRespSeleccionado() {
+        return respSeleccionado;
+    }
+
+    public void setRespSeleccionado(Responsables respSeleccionado) {
+        this.respSeleccionado = respSeleccionado;
+    }
+
+    public List<Responsables> getRespList() {
+        return respList;
+    }
+
+    public void setRespList(List<Responsables> respList) {
+        this.respList = respList;
+    }
+    
+    public void obtenerTodos(){
+        FacesContext contexto = FacesContext.getCurrentInstance();
+        try {
+            respList = responsablesFacade.findAll();
+        } catch (Exception e) {
+            contexto.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_FATAL, "Error!", e.getMessage()));
+        }
+    }
+    
+    public void crearResponsable(){
+        FacesContext contexto = FacesContext.getCurrentInstance();
+        try {
+            boolean creado = responsablesFacade.create(resp);
+            if (creado) {
+                resp = new Responsables();
+                contexto.addMessage(null, new FacesMessage("Registro guardado"));
+            } else {
+                contexto.addMessage(null, new FacesMessage("No se pudo guardar el registro!"));
+            }
+            obtenerTodos();
+        } catch (Exception e) {
+            contexto.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_FATAL, "Error!", e.getMessage()));
+        }
+    }
+    
+    public void editarResponsable(){
+        FacesContext contexto = FacesContext.getCurrentInstance();
+        try {
+            boolean modificado = responsablesFacade.edit(respSeleccionado);
+            if (modificado) {
+                contexto.addMessage(null, new FacesMessage("Registro modificado"));
+            } else {
+                contexto.addMessage(null, new FacesMessage("No se pudo modificar el registro!"));
+            }
+            obtenerTodos();
+        } catch (Exception e) {
+            contexto.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_FATAL, "Error!", e.getMessage()));
+        }
+    }
+    
+     public void eliminarResponsable(){
+        FacesContext contexto = FacesContext.getCurrentInstance();
+        try {
+            boolean eliminado = responsablesFacade.remove(respSeleccionado);
+            if (eliminado) {
+                contexto.addMessage(null, new FacesMessage("Registro eliminado"));
+            } else {
+                contexto.addMessage(null, new FacesMessage("No se pudo eliminar el registro!"));
+            }
+            obtenerTodos();
+        } catch (Exception e) {
+            contexto.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_FATAL, "Error!", e.getMessage()));
+        }
+    }
 }
