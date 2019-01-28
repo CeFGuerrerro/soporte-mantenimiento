bd6a21e2862c4585781be8ff62bfb54588e155e6
Cybertron <Cybertron@DESKTOP-SH9RJHA>
2019-01-28 17:21:34 -0600

    Casi terminada la de SolicitudUsuarioForm, pruebenla :v

--- soporte-mantenimiento/soporte-mantenimiento-war/src/java/edu/sv/uesocc/controladores/SolicitudUsuarioMB.java
+++ soporte-mantenimiento/soporte-mantenimiento-war/src/java/edu/sv/uesocc/controladores/SolicitudUsuarioMB.java
@@ -2,12 +2,16 @@ package edu.sv.uesocc.controladores;
 
 import edu.sv.uesocc.entidades.Equipos;
 import edu.sv.uesocc.entidades.Responsables;
+import edu.sv.uesocc.entidades.Solicitudes;
+import edu.sv.uesocc.entidades.Tipo;
 import edu.sv.uesocc.entidades.Usuarios;
 import edu.sv.uesocc.facades.EquiposFacadeLocal;
 import edu.sv.uesocc.facades.ResponsablesFacadeLocal;
+import edu.sv.uesocc.facades.SolicitudesFacadeLocal;
 import edu.sv.uesocc.facades.UsuariosFacadeLocal;
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
@@ -15,7 +19,6 @@ import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
 import javax.faces.view.ViewScoped;
 import javax.inject.Named;
-import org.primefaces.PrimeFaces;
 
 @Named(value = "solicitudUsuarioMB")
 @ViewScoped
@@ -27,13 +30,19 @@ public class SolicitudUsuarioMB implements Serializable {
     private UsuariosFacadeLocal usuarioEJB;
     @EJB
     private ResponsablesFacadeLocal respEJB;
+    @EJB
+    private SolicitudesFacadeLocal soliciEJB;
 
+    private Solicitudes solici = new Solicitudes();
     private Usuarios usuario = new Usuarios();
     private Equipos equp = new Equipos();
     private Responsables resp = new Responsables();
     private List<Equipos> equipList = new ArrayList<>();
     private List<Responsables> respList = new ArrayList<>();
+    private List<Tipo> tipoList;
     private int paso = 0;
+    private int tipo;
+    private boolean tipoProblema;
     private boolean paso1 = true;
     private boolean paso2;
     private boolean paso3;
@@ -42,7 +51,11 @@ public class SolicitudUsuarioMB implements Serializable {
 
     @PostConstruct
     public void init() {
+        tipoList = Arrays.asList(new Tipo(1,"Problema con mi equipo (UES)"), new Tipo (2,"Problema un equipo en la Unidad/Area"), new Tipo(2,"Otro..."));
+    }
 
+    public List<Tipo> getTipoList() {
+        return tipoList;
     }
 
     public List<Responsables> getRespList() {
@@ -59,6 +72,22 @@ public class SolicitudUsuarioMB implements Serializable {
 
     public void setEquipList(List<Equipos> equipList) {
         this.equipList = equipList;
+    }
+
+    public int getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(int tipo) {
+        this.tipo = tipo;
+    }
+
+    public boolean isTipoProblema() {
+        return tipoProblema;
+    }
+
+    public void setTipoProblema(boolean tipoProblema) {
+        this.tipoProblema = tipoProblema;
     }
 
     public int getPaso() {
@@ -101,6 +130,14 @@ public class SolicitudUsuarioMB implements Serializable {
         this.paso4 = paso4;
     }
 
+    public Solicitudes getSolici() {
+        return solici;
+    }
+
+    public void setSolici(Solicitudes solici) {
+        this.solici = solici;
+    }
+
     public boolean isEquipo() {
         return equipo;
     }
@@ -125,9 +162,20 @@ public class SolicitudUsuarioMB implements Serializable {
         this.resp = resp;
     }
 
+    public void tipoProb() {
+        resp = new Responsables();
+        equp = new Equipos();
+        tipoProblema = true;
+    }
+
     public void resposableEquipo() {
-        equipo = true;
+        equp = new Equipos();
         equipList = equipoEJB.findPorResponsable(resp);
+        if (equipList.size() > 1) {
+            equipo = true;
+        } else {
+            equp = equipList.get(0);
+        }
     }
 
     public void obtenerResponsable() {
@@ -154,6 +202,7 @@ public class SolicitudUsuarioMB implements Serializable {
 
     public void irPaso2() {
         FacesContext contexto = FacesContext.getCurrentInstance();
+        //Comprueba si viene del paso 1
         if (paso1) {
             boolean rs = usuarioEJB.iniciarSesion(usuario);
             if (rs) {
@@ -171,8 +220,12 @@ public class SolicitudUsuarioMB implements Serializable {
             this.paso2 = true;
             this.paso3 = false;
             this.equipo = false;
+            tipo = 0;
+            tipoProblema = false;
+            solici = new Solicitudes();
             equp = new Equipos();
             resp = new Responsables();
+            solici = new Solicitudes();
         }
     }
 
@@ -187,5 +240,9 @@ public class SolicitudUsuarioMB implements Serializable {
         this.paso = 3;
         this.paso3 = false;
         this.paso4 = true;
+    }
+    
+    public void enviar(){
+        
     }
 }
